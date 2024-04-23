a2182800f96ad09b419d6a51b61a0d18b0950cd5
Dove6 <24943032+Dove6@users.noreply.github.com>
2024-04-23 03:21:33 +0200

    Add streaming cipher decoder
    with implementation based on main pixlib package

--- pixlib_parser/src/main.rs
+++ pixlib_parser/src/main.rs
@@ -1,9 +1,14 @@
-use std::{io::Read, path::PathBuf};
+use lazy_static::lazy_static;
+use regex::bytes::Regex;
+use std::{
+    io::Read,
+    path::PathBuf,
+};
 
 use pixlib_parser::{
     common::{Issue, IssueHandler, IssueManager},
     declarative_parser::{DeclarativeParser, ParserIssue},
-    scanner::{CnvScanner, CodepageDecoder, CP1250_LUT},
+    scanner::{CnvDecoder, CnvScanner, CodepageDecoder, CP1250_LUT},
 };
 
 #[derive(Debug)]
@@ -15,10 +20,34 @@ impl<I: Issue> IssueHandler<I> for IssuePrinter {
     }
 }
 
+lazy_static! {
+    static ref CNV_HEADER_REGEX: Regex = Regex::new(r"^\{<([CD]):(\d{1,10})>\}\s+$")
+        .expect("The regex for CNV header should be defined correctly.");
+}
+
 fn parse_declarative(filename: PathBuf) -> std::io::Result<()> {
     eprintln!("{:?}", &filename);
     let input = std::fs::File::open(filename)?;
-    let decoder = CodepageDecoder::new(&CP1250_LUT, input.bytes());
+    let mut input = input.bytes().peekable();
+    let mut first_line = Vec::<u8>::new();
+    while let Some(res) = input.next_if(|res| res.as_ref().is_ok_and(|c| !matches!(c, b'\r' | b'\n'))) {
+        first_line.push(res.unwrap())
+    }
+    while let Some(res) = input.next_if(|res| res.as_ref().is_ok_and(|c| matches!(c, b'\r' | b'\n'))) {
+        first_line.push(res.unwrap())
+    }
+    let input: Box<dyn Iterator<Item = std::io::Result<u8>>> =
+        if let Some(m) = CNV_HEADER_REGEX.captures(&first_line) {
+            let _cipher_class = m[1][0] as char;
+            let step_count = m[2].iter().rev().enumerate().fold(0, |acc, (i, n)| {
+                acc + ((*n - 48) as usize) * (10 as usize).pow(i.try_into().unwrap())
+            });
+            println!("{}, {}", _cipher_class, step_count);
+            Box::new(CnvDecoder::new(input, step_count))
+        } else {
+            Box::new(first_line.into_iter().map(|c| Ok(c)).chain(input))
+        };
+    let decoder = CodepageDecoder::new(&CP1250_LUT, input);
     let scanner = CnvScanner::new(decoder);
     let mut parser_issue_manager: IssueManager<ParserIssue> = Default::default();
     parser_issue_manager.set_handler(Box::new(IssuePrinter));
--- pixlib_parser/src/scanner.rs
+++ pixlib_parser/src/scanner.rs
@@ -1,4 +1,414 @@
+use std::collections::VecDeque;
+
 use crate::common::{Position, Spanned};
+
+type IoReadResult = std::io::Result<u8>;
+type ScannerInput = std::io::Result<char>;
+type ScannerOutput = Spanned<char, Position, std::io::Error>;
+
+#[derive(Clone, Debug)]
+struct IterBuf<I: Iterator<Item = IoReadResult>> {
+    input: I,
+    buffer: VecDeque<u8>,
+    capacity: usize,
+}
+
+impl<I: Iterator<Item = IoReadResult>> IterBuf<I> {
+    pub fn with_capacity(capacity: usize, input: I) -> Self {
+        Self {
+            input,
+            capacity,
+            buffer: VecDeque::with_capacity(capacity),
+        }
+    }
+
+    pub fn refill(&mut self) -> std::io::Result<()> {
+        while self.buffer.len() < self.capacity {
+            if let Some(next) = self.input.next() {
+                self.buffer.push_back(next?);
+            } else {
+                break;
+            }
+        }
+        Ok(())
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.buffer.is_empty()
+    }
+
+    #[allow(dead_code)]
+    pub fn len(&self) -> usize {
+        self.buffer.len()
+    }
+
+    pub fn peek(&self) -> Option<u8> {
+        if self.buffer.is_empty() {
+            None
+        } else {
+            Some(self.buffer[0])
+        }
+    }
+
+    pub fn advance(&mut self) -> std::io::Result<()> {
+        if self.buffer.is_empty() {
+            if let Some(result) = self.input.next() {
+                result?;
+            }
+        } else {
+            self.buffer.pop_front();
+        }
+        self.refill()?;
+        Ok(())
+    }
+
+    pub fn advance_n(&mut self, n: usize) -> std::io::Result<()> {
+        let old_len = self.buffer.len();
+        if n >= old_len {
+            self.buffer.clear();
+            for _ in 0..(n - old_len) {
+                if let Some(result) = self.input.next() {
+                    result?;
+                } else {
+                    break;
+                }
+            }
+        } else {
+            for _ in 0..n {
+                self.buffer.pop_front();
+            }
+        }
+        self.refill()?;
+        Ok(())
+    }
+
+    pub fn starts_with(&self, bytes: &[u8]) -> bool {
+        if bytes.is_empty() {
+            return true;
+        }
+        if self.buffer.len() < bytes.len() {
+            return false;
+        }
+        let slices = self.buffer.as_slices();
+        if bytes.len() <= slices.0.len() {
+            slices.0[..bytes.len()] == *bytes
+        } else {
+            *slices.0 == bytes[..slices.0.len()]
+                && slices.1[..(bytes.len() - slices.0.len())] == bytes[slices.0.len()..]
+        }
+    }
+}
+
+impl<I: Iterator<Item = IoReadResult>> PartialEq<&[u8]> for IterBuf<I> {
+    fn eq(&self, other: &&[u8]) -> bool {
+        self.buffer == *other
+    }
+}
+
+#[cfg(test)]
+mod buf_iter_tests {
+    use super::*;
+
+    #[test]
+    fn buffer_should_be_empty_just_after_creation() {
+        let input = "1234567890";
+        let capacity = 2;
+        let iter_buf = IterBuf::with_capacity(capacity, input.bytes().map(|b| Ok(b)).into_iter());
+
+        assert!(iter_buf.is_empty());
+        assert_eq!(iter_buf.peek(), None);
+    }
+
+    #[test]
+    fn refill_should_refill_the_buffer_if_possible() {
+        let input = "1234567890";
+        let capacity = 2;
+        let mut iter_buf =
+            IterBuf::with_capacity(capacity, input.bytes().map(|b| Ok(b)).into_iter());
+        assert!(iter_buf.refill().is_ok());
+
+        assert!(!iter_buf.is_empty());
+        assert_eq!(iter_buf.len(), capacity);
+        assert_eq!(iter_buf.peek(), Some(b'1'));
+        assert_eq!(iter_buf, b"12".as_ref());
+    }
+
+    #[test]
+    fn advance_should_move_full_buffer_by_one_byte() {
+        let input = "1234567890";
+        let capacity = 2;
+        let mut iter_buf =
+            IterBuf::with_capacity(capacity, input.bytes().map(|b| Ok(b)).into_iter());
+        iter_buf.refill().unwrap();
+        assert!(iter_buf.advance().is_ok());
+
+        assert!(!iter_buf.is_empty());
+        assert_eq!(iter_buf.len(), capacity);
+        assert_eq!(iter_buf.peek(), Some(b'2'));
+        assert_eq!(iter_buf, b"23".as_ref());
+    }
+
+    #[test]
+    fn advance_should_skip_one_byte_and_refill_if_the_buffer_is_empty() {
+        let input = "1234567890";
+        let capacity = 2;
+        let mut iter_buf =
+            IterBuf::with_capacity(capacity, input.bytes().map(|b| Ok(b)).into_iter());
+        assert!(iter_buf.advance().is_ok());
+
+        assert!(!iter_buf.is_empty());
+        assert_eq!(iter_buf.len(), capacity);
+        assert_eq!(iter_buf.peek(), Some(b'2'));
+        assert_eq!(iter_buf, b"23".as_ref());
+    }
+
+    #[test]
+    fn advance_n_with_n_equal_to_1_should_work_just_as_advance_for_full_buffer() {
+        let input = "1234567890";
+        let capacity = 2;
+        let mut iter_buf =
+            IterBuf::with_capacity(capacity, input.bytes().map(|b| Ok(b)).into_iter());
+        iter_buf.refill().unwrap();
+        assert!(iter_buf.advance_n(1).is_ok());
+
+        assert!(!iter_buf.is_empty());
+        assert_eq!(iter_buf.len(), capacity);
+        assert_eq!(iter_buf.peek(), Some(b'2'));
+        assert_eq!(iter_buf, b"23".as_ref());
+    }
+
+    #[test]
+    fn advance_n_with_n_equal_to_1_should_work_just_as_advance_for_empty_buffer() {
+        let input = "1234567890";
+        let capacity = 2;
+        let mut iter_buf =
+            IterBuf::with_capacity(capacity, input.bytes().map(|b| Ok(b)).into_iter());
+        assert!(iter_buf.advance_n(1).is_ok());
+
+        assert!(!iter_buf.is_empty());
+        assert_eq!(iter_buf.len(), capacity);
+        assert_eq!(iter_buf.peek(), Some(b'2'));
+        assert_eq!(iter_buf, b"23".as_ref());
+    }
+
+    #[test]
+    fn advance_n_should_work_correctly_for_n_equal_to_capacity_and_full_buffer() {
+        let input = "1234567890";
+        let capacity = 2;
+        let mut iter_buf =
+            IterBuf::with_capacity(capacity, input.bytes().map(|b| Ok(b)).into_iter());
+        iter_buf.refill().unwrap();
+        assert!(iter_buf.advance_n(capacity).is_ok());
+
+        assert!(!iter_buf.is_empty());
+        assert_eq!(iter_buf.len(), capacity);
+        assert_eq!(iter_buf.peek(), Some(b'3'));
+        assert_eq!(iter_buf, b"34".as_ref());
+    }
+
+    #[test]
+    fn advance_n_should_work_correctly_for_n_equal_to_capacity_and_empty_buffer() {
+        let input = "1234567890";
+        let capacity = 2;
+        let mut iter_buf =
+            IterBuf::with_capacity(capacity, input.bytes().map(|b| Ok(b)).into_iter());
+        assert!(iter_buf.advance_n(capacity).is_ok());
+
+        assert!(!iter_buf.is_empty());
+        assert_eq!(iter_buf.len(), capacity);
+        assert_eq!(iter_buf.peek(), Some(b'3'));
+        assert_eq!(iter_buf, b"34".as_ref());
+    }
+
+    #[test]
+    fn advance_n_should_work_correctly_for_n_above_capacity() {
+        let input = "1234567890";
+        let capacity = 2;
+        let mut iter_buf =
+            IterBuf::with_capacity(capacity, input.bytes().map(|b| Ok(b)).into_iter());
+        iter_buf.refill().unwrap();
+        assert!(iter_buf.advance_n(3).is_ok());
+
+        assert!(!iter_buf.is_empty());
+        assert_eq!(iter_buf.len(), capacity);
+        assert_eq!(iter_buf.peek(), Some(b'4'));
+        assert_eq!(iter_buf, b"45".as_ref());
+    }
+
+    #[test]
+    fn advance_n_should_work_correctly_for_n_above_capacity_and_empty_buffer() {
+        let input = "1234567890";
+        let capacity = 2;
+        let mut iter_buf =
+            IterBuf::with_capacity(capacity, input.bytes().map(|b| Ok(b)).into_iter());
+        assert!(iter_buf.advance_n(3).is_ok());
+
+        assert!(!iter_buf.is_empty());
+        assert_eq!(iter_buf.len(), capacity);
+        assert_eq!(iter_buf.peek(), Some(b'4'));
+        assert_eq!(iter_buf, b"45".as_ref());
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Default)]
+struct CnvDecoderState {
+    position: usize,
+    current_step: u16,
+    newline_counter: u8,
+}
+
+#[derive(Clone, Debug)]
+pub struct CnvDecoder<I: Iterator<Item = IoReadResult>> {
+    input: IterBuf<I>,
+    steps: u16,
+    state: CnvDecoderState,
+}
+
+static NEWLINE_TOKEN: &[u8; 3] = b"<E>";
+
+impl<I: Iterator<Item = IoReadResult>> CnvDecoder<I> {
+    #[allow(dead_code)]
+    pub fn new(input: I, steps: usize) -> Self {
+        Self {
+            input: IterBuf::with_capacity(NEWLINE_TOKEN.len(), input),
+            steps: steps as u16,
+            state: Default::default(),
+        }
+    }
+
+    #[allow(dead_code)]
+    pub fn get_position(&self) -> usize {
+        self.state.position
+    }
+
+    fn try_consume_newline(&mut self) -> std::io::Result<bool> {
+        if self.input.starts_with(NEWLINE_TOKEN.as_ref()) {
+            self.state.position += NEWLINE_TOKEN.len();
+            self.state.newline_counter += 1;
+            self.input.advance_n(NEWLINE_TOKEN.len())?;
+            Ok(true)
+        } else {
+            Ok(false)
+        }
+    }
+
+    fn try_decode_byte_and_advance(&mut self) -> std::io::Result<Option<u8>> {
+        let shift = ((self.state.current_step >> 1) + 1) as isize;
+        let shift = shift
+            * if self.state.current_step % 2 == 0 {
+                -1
+            } else {
+                1
+            };
+        let Some(decoded_byte) = self.input.peek() else {
+            return Ok(None);
+        };
+        let decoded_byte = decoded_byte as isize + shift;
+        self.state.current_step = (self.state.current_step + 1) % self.steps;
+        self.input.advance()?;
+        Ok(Some(decoded_byte as u8))
+    }
+}
+
+impl<I: Iterator<Item = IoReadResult>> Iterator for CnvDecoder<I> {
+    type Item = IoReadResult;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.input.is_empty() {
+            if let Err(e) = self.input.refill() {
+                return Some(Err(e));
+            }
+            if self.input.is_empty() {
+                return None;
+            }
+        }
+        match self.try_consume_newline() {
+            Err(e) => return Some(Err(e)),
+            Ok(true) => {
+                while matches!(self.input.peek(), Some(b'\n') | Some(b'\r')) {
+                    self.state.position += 1;
+                    if let Err(e) = self.input.advance() {
+                        return Some(Err(e));
+                    }
+                }
+                if self.state.newline_counter == 6 {
+                    self.state.newline_counter = 0;
+                }
+                return Some(Ok(b'\n'));
+            }
+            Ok(false) => (),
+        }
+        match self.try_decode_byte_and_advance() {
+            Err(e) => return Some(Err(e)),
+            Ok(opt) => opt.map(|b| Ok(b)),
+        }
+    }
+}
+
+#[cfg(test)]
+mod cnv_decoder_tests {
+    use super::*;
+
+    #[test]
+    fn simple_text_should_be_decoded_correctly() {
+        let input = b"PALCFQ".as_ref();
+        let decoder = CnvDecoder::new(input.iter().map(|b| Ok(*b)), 6);
+        assert_eq!(
+            decoder.map(|res| res.unwrap()).collect::<Vec<_>>(),
+            b"OBJECT"
+        );
+    }
+
+    #[test]
+    fn simple_text_with_newline_marker_should_be_decoded_correctly() {
+        let input = b"PAL<E>CFQ".as_ref();
+        let decoder = CnvDecoder::new(input.iter().map(|b| Ok(*b)), 6);
+        assert_eq!(
+            decoder.map(|res| res.unwrap()).collect::<Vec<_>>(),
+            b"OBJ\nECT"
+        );
+    }
+
+    #[test]
+    fn simple_text_with_newline_marker_and_newlines_should_be_decoded_correctly() {
+        let input = b"PAL<E>\r\nCFQ".as_ref();
+        let decoder = CnvDecoder::new(input.iter().map(|b| Ok(*b)), 6);
+        assert_eq!(
+            decoder.map(|res| res.unwrap()).collect::<Vec<_>>(),
+            b"OBJ\nECT"
+        );
+    }
+
+    #[test]
+    fn newlines_without_newline_marker_preceding_them_should_be_decoded_literally() {
+        let input = b"PAL\r\nC".as_ref();
+        let decoder = CnvDecoder::new(input.iter().map(|b| Ok(*b)), 6);
+        assert_eq!(
+            decoder.map(|res| res.unwrap()).collect::<Vec<_>>(),
+            b"OBJ\x0f\x07F"
+        );
+    }
+
+    #[test]
+    fn step_count_should_be_reset_after_reaching_max_step() {
+        let input = b"BKC\x1eP>!JQRD".as_ref();
+        let decoder = CnvDecoder::new(input.iter().map(|b| Ok(*b)), 6);
+        assert_eq!(
+            decoder.map(|res| res.unwrap()).collect::<Vec<_>>(),
+            b"ALA MA KOTA"
+        );
+    }
+
+    #[test]
+    fn several_newline_markers_should_be_decoded_correctly() {
+        let input = b"A<E><E><E><E>B".as_ref();
+        let decoder = CnvDecoder::new(input.iter().map(|b| Ok(*b)), 6);
+        assert_eq!(
+            decoder.map(|res| res.unwrap()).collect::<Vec<_>>(),
+            b"@\n\n\n\nC"
+        );
+    }
+}
 
 lazy_static::lazy_static! {
     pub static ref CP1250_LUT: [char; 128] = [
@@ -21,10 +431,6 @@ lazy_static::lazy_static! {
     ].map(|x| char::from_u32(x).unwrap_or_else(|| panic!("Unexpected value: {}. \
         Codepage entries should consist of valid Unicode characters only.", x)));
 }
-
-type IoReadResult = std::io::Result<u8>;
-type ScannerInput = std::io::Result<char>;
-type ScannerOutput = Spanned<char, Position, std::io::Error>;
 
 pub struct CodepageDecoder<'lut, I: Iterator<Item = IoReadResult>> {
     /// A table for mapping all bytes with the most significant bit set to Unicode chars.
